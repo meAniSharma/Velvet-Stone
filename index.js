5a7aa21b29cd6304e930a281f652aa87b2e65af0
Adit Sharma <aditsharma123@gmail.com>
2025-04-27 19:02:22 +0530

    Updated home page js

---
+++ index.js
@@ -0,0 +1,28 @@
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+  }
+  
+  function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+  }
+  
+  window.onclick = function(event) {
+    const loginModal = document.getElementById('loginModal');
+    const signupModal = document.getElementById('signupModal');
+    if (event.target == loginModal) {
+      loginModal.style.display = "none";
+    }
+    if (event.target == signupModal) {
+      signupModal.style.display = "none";
+    }
+  }
+  function clearSearch() {
+  const searchInputs = document.querySelectorAll('.search-bar input, .search-bar select');
+  searchInputs.forEach(input => {
+    if (input.tagName === 'SELECT') {
+      input.selectedIndex = 0;
+    } else {
+      input.value = '';
+    }
+  });
+}
